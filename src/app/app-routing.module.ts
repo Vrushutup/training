7e0314018a09402dcbd01cd83a5176aff7a308fb
Vrushabh Shamkuwar <vrushabh@hudsondata.com>
2022-05-11 21:38:54 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+// import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
+// import { ExpenseEntryListComponent } from './expense-entry-list/expense-entry-list.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
-const routes: Routes = [];
+import { ExpenseGuard } from './expense.guard';
+
+const routes: Routes = [
+   { path: 'login', component: LoginComponent },
+   { path: 'logout', component: LogoutComponent },
+  //  { path: 'expenses', component: ExpenseEntryListComponent, canActivate: [ExpenseGuard]},
+  //  { path: 'expenses/detail/:id', component: ExpenseEntryComponent, canActivate: [ExpenseGuard]},
+   { path: '', redirectTo: 'expenses', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
